fac2cd56140d79be9ebff7f33a9dbc9248c5d868
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-05-10 11:32:24 +0000

    Item12493: Numerous small niggles in Empty* templates fixed. Removed dead EmptyTag.
    git-svn-id: http://svn.foswiki.org/trunk/EmptyContrib@16712 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/EmptyContrib/EmptyContribSuite.pm
+++ test/unit/EmptyContrib/EmptyContribSuite.pm
@@ -5,6 +5,8 @@ our @ISA = qw( Unit::TestSuite );
 
 sub name { 'EmptyContribSuite' }
 
+# List the modules that contain the extension-specific tests you
+# want to run. These tests are run when you 'perl build.pl test'
 sub include_tests { qw(EmptyContribTests) }
 
 1;
